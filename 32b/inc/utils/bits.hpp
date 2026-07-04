f1aaa48ab969fefabc01a5e982efd2ac7689aa42
Tom Morgan <tomos.morgan01@gmail.com>
2026-07-04 16:24:08 +0100

    Bits.hpp inline functions

--- 32b/inc/utils/bits.hpp
+++ 32b/inc/utils/bits.hpp
@@ -4,13 +4,13 @@
 
 namespace bits
 {
-int32_t sign_extend(uint32_t value, uint32_t bits)
+inline int32_t sign_extend(uint32_t value, uint32_t bits)
 {
 	uint32_t sign_bit = 1 << (bits - 1);
 	return (value ^ sign_bit) - sign_bit;
 }
 
-uint32_t get_bits(uint32_t value, uint32_t start, uint32_t length)
+inline uint32_t get_bits(uint32_t value, uint32_t start, uint32_t length)
 {
 	uint32_t mask = (1 << length) - 1;
 	return (value >> start) & mask;
